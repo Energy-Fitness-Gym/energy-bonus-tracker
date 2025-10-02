655b86ecf612c1789976bcdff6b0f46b3c9e7287
JusteJonathan <jfrejuste34@gmail.com>
2025-10-02 08:18:49 -0400

    Initial commit with starter files

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Energy Bonus Tracker", layout="centered")
+
+st.title("Energy Bonus Tracker")
+st.write("🚧 Work in progress... Manager submission and employee dashboards coming soon!")
